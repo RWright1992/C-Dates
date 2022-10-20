b35887cfcce99bc3b5a05234db7ec467f912083f
RWright1992 <ryanwright1992@gmail.com>
2022-10-20 21:26:20 +0100

    adding all functionality for app - RW

--- Program.cs
+++ Program.cs
@@ -6,23 +6,33 @@ public class Program
 	public static void Main()
 	{
 		Console.WriteLine("Enter your date of birth (ddmmyyyy)"); //Message to be displayed to user
-		var userInput = Console.ReadLine(); //setting var
+		var userInput = Console.ReadLine(); //setting var for user input
 		DateTime today = DateTime.Today; //setting Datetime set to today
+		DateTime eighteenYearsAgo = today.AddYears(-18); //setting DateTime for 18 years ago
 		DateTime dateOfBirth; //setting datetime to parse user input into
 		DateTime.TryParseExact(userInput, "ddMMyyyy", null,DateTimeStyles.None,out dateOfBirth); //parsing user input
 		DateTime nextBirthday = new DateTime(today.Year, dateOfBirth.Month, dateOfBirth.Day); //creating next birthday datetime
 		
 		if(!dateOfBirth.Equals(DateTime.MinValue)) //validating that date entered
 		{
-  			if (nextBirthday < today) //if birthday has passed in year add a year
+  			if (nextBirthday < today)
 			{
 				nextBirthday = nextBirthday.AddYears(1);
 			};
 			
-			int daysToBirthday = (nextBirthday - today).Days; //calculating day to next birthday
-			int daysFromBirth = (today - dateOfBirth).Days; //calculating days from birth
+			int daysToBirthday = (nextBirthday - today).Days;
+			int daysFromBirth = (today - dateOfBirth).Days;
 			Console.WriteLine("Days to Birthday:"+ daysToBirthday);
-			Console.WriteLine("Days from birth:"+ daysFromBirth); 
+			Console.WriteLine("Days from birth:"+ daysFromBirth);
+			if (dateOfBirth > eighteenYearsAgo)
+			{
+				Console.WriteLine("Under 18");
+			}
+			else
+			{
+				Console.WriteLine("Over 18");
+			};
+			
 		}
 	}
 }
